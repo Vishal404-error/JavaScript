f77c3b6775f5e82c85305fe73e2173953db21b3f
vishal <114876887+Vishal404-error@users.noreply.github.com>
2024-02-28 10:23:46 +0000

    operations

---
+++ 01_Basics/operations.js
@@ -0,0 +1,32 @@
+let value=3
+
+let negValue= -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1="vishal"
+let str2 =" singh"
+
+// console.log(str1 +str2);  // concatenation
+
+
+// console.log("1"+ 2);  //12
+// console.log(1+"2");    //12
+// console.log("1"+"2");   //12
+// console.log("1"+2 +"2");   //122
+// console.log(1+2+"2");    //32
+
+
+// console.log(+true);   //1
+// console.log(+"");     //0
+
+
+let gameCount=100
+gameCount++
+console.log(gameCount);
