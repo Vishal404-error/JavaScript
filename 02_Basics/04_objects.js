46f3c20e018d65407e446f6f34dfae3519a5b114
vishal <114876887+Vishal404-error@users.noreply.github.com>
2024-03-27 12:10:56 +0000

    destructuring and json

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,95 @@
+//const tinderUser= new Object();  // singelton object
+
+const tinderUser={}                 //non-singelton object
+
+tinderUser.id="123abc"
+tinderUser.name="vishal"
+tinderUser.isLoggedIn=false
+
+// console.log(tinderUser);
+
+const regularUser ={
+    email: "vishal@123",
+    fullname :{
+        userfullname : {
+            firstname: "vishal",
+            lastname : "singh"
+        }
+    }
+}
+
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.userfullname);
+
+const obj1={ 1:"a",2:"b"}
+
+const obj2={ 3:"a",4:"b"}
+
+// const obj3= {obj1,obj2}
+
+
+//const obj3 =Object.assign({},obj1,obj2)     //  { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+
+const obj3 ={...obj1,...obj2}        //  using spread  { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+//console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "a@gmail.com"
+    },
+    {
+        id: 2,
+        email: "b@gmail.com"
+    },
+    {
+        id: 3,
+        email: "c@gmail.com"
+    },
+]
+
+users[1].email
+console.log(users[0].email);
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+// DE-STRUCTUREING
+
+
+const course = {
+    coursename : "xyz",
+    price : "999",
+    courseInstructor:" alpha"
+
+}
+
+//  to access  courseInstructor
+//  course.courseInstructor
+
+ const {courseInstructor: instructor} = course   //syntax  for destructuring
+ console.log(instructor);
+
+
+
+ // JSON  syntax
+
+//  {
+//     "name": "vishal",
+//     "coursename": "bvjs",
+//     "price" : "free"
+//  }
+
+
+//  [
+//     {},
+//     {},
+//     {}
+// ]
